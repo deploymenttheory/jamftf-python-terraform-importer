b37538e34d4a1c8fff7c0caa65024e665eb39397
Joseph Little <joelittle11@live.com>
2024-08-16 12:59:33 +0100

    moving func into importer obj

--- jamftf/importer.py
+++ jamftf/importer.py
@@ -1,7 +1,20 @@
 from .resources.script import Script
 
 class Importer:
+    targetted = []
     def __init__(self, client, use_jamf_name: bool):
         self.scripts = Script(client, use_jamf_name)
+        self.targetted = [Script(client, use_jamf_name)]
+
+
+
+    def HCL(self):
+        hcl = ""
+        for res in self.targetted:
+            hcl += res.HCL()
+
+        return hcl
+
+            
 
 
