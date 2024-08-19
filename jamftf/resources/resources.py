60ade988d7f168e36e06564daee3be1079143331
Joseph Little <joelittle11@live.com>
2024-08-19 16:06:26 +0100

    bugfix bump

--- jamftf/resources/resources.py
+++ jamftf/resources/resources.py
@@ -65,12 +65,13 @@ class Resource:
 
         # Remove duplicates
         if len(self.options.exclude_ids) > 0:
-            print("One")
+            to_delete = []
             for i in self._data:
-                print("loop")
                 if int(self._data[i]["id"]) in self.options.exclude_ids:
-                    print("Two")
-                    del self._data[i]
+                    to_delete.append(i)
+
+            for i in to_delete:
+                del self._data[i]
 
         # Name change
         if self.options.use_resource_type_as_name:
--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name="jamftf",
     packages=find_packages(),
-    version="0.0.25"
+    version="0.0.26"
 )
