1fb30b3289b3f8d9d245417e1aa0e7814079b5c1
Joseph Little <joelittle11@live.com>
2024-08-21 15:45:48 +0100

    bugfix and bump

--- jamftf/constants.py
+++ jamftf/constants.py
@@ -4,9 +4,9 @@ ILLEGAL_NAME_CHARS = [".", "/"]
 
 # terraform resource type strings centralised
 RESOURCE_TYPES = {
-    "scripts": "jamfpro_script",
-    "categories": "jamfpro_categorie",
-    "departments": "jamfpro_department"
+    "script": "jamfpro_script",
+    "category": "jamfpro_categorie",
+    "department": "jamfpro_department"
 }
 
 ALL_RESOURCE_TYPES = list(RESOURCE_TYPES.values())
