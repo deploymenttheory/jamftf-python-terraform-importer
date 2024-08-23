ff9a09b76c4ef0ac9101e94f23689e1109a2a788
Joseph Little <joelittle11@live.com>
2024-08-23 15:41:07 +0100

    added prettfier

--- jamftf/importer.py
+++ jamftf/importer.py
@@ -4,6 +4,8 @@ import jamfpy
 from .exceptions import ImporterConfigError, OptionsConflictError
 from .resources import Resource
 from .options import Options
+from random import randint
+import os
 
 class Importer:
     """object for managing all targetted resources"""
@@ -29,10 +31,24 @@ class Importer:
             t.refresh_data()
 
 
-    def HCL(self):
+    def HCL(self, pretty=False):
         """generates hcl on every targetted object"""
         out = ""
         for r in self.targetted:
             out += "\n" + "\n".join(r.build_hcl()) + "\n"
 
+        pretty_hcl = ""
+        fn = f"_temp_{randint(1, 9999)}"
+        if pretty:
+            with open(fn, "w") as f:
+                f.write(out)
+
+            os.system("terraform fmt")
+
+            with open(fn, "r") as f:
+                pretty_hcl = f.read()
+
+            os.remove(fn)
+
+        out = pretty_hcl
         return out
