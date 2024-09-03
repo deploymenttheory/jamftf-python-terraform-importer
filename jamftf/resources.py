c4634129f4e66d4661fc8ae21e4472253ecdf0e6
Joseph Little <joelittle11@live.com>
2024-09-03 09:31:03 +0100

    debugging

--- jamftf/resources.py
+++ jamftf/resources.py
@@ -100,12 +100,13 @@ class Resource:
     def set_debug(self, debug: bool):
         level = self._init_log_level(debug)
         self.lg.setLevel(level)
+        for i in self.lg.handlers:
+            i.setLevel(level)
         self.lg.info(f"log level has been overridden to: {self.lg.level}")
 
 
     def set_client(self, client: jamfpy.JamfTenant, refresh_data: bool = False):
         """function to wrap setting of object bound client"""
-        print(f"DEBUG: {self.lg.level}")
         self.lg.debug("setting client...")
 
         assert isinstance(client, jamfpy.JamfTenant), "invalid client type"
