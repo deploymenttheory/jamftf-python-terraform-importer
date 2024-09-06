af7906f91be8342452c376d13ffee5b6632b1b2e
Joseph Little <joelittle11@live.com>
2024-09-06 12:01:07 +0100

    adding debug prints

--- jamftf/options.py
+++ jamftf/options.py
@@ -95,6 +95,7 @@ class Applicator:
     def _exclude_ids(self, data: dict) -> dict:
         """removes any IDs from the data which have been specifid to be excluded"""
         self.lg.debug("excluding ids... %s", self.exclude_ids)
+        print("HERE")
 
         to_delete = []
         for i in data:
