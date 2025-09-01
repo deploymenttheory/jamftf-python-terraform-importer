779a7cd631f5fb7381a4a3cfa491ee1820f131ec
Joseph <joelittle11@live.com>
2025-09-01 11:39:06 +0100

    clearer definition of a space

--- jamftf/hcl.py
+++ jamftf/hcl.py
@@ -2,6 +2,8 @@
 handles all hcl related operations
 """
 
+SPACE = "\x20"
+
 from typing import List
 from .dataclasses import SingleItem
 
@@ -10,10 +12,12 @@ def import_block(resource_type, jpro_id):
     Return a Terraform import block string for a resource.
     """
 
+    # Unsure if I'm a fan of how these strings are constructed but it improves clarity
+    # on exactly which characters are being returned and how many of them.
     return (
         f"import {{\n"
-        f"  id = {jpro_id}\n"
-        f"  to = {resource_type.value}.{resource_type.value}-{jpro_id}\n"
+        f"{2 * SPACE}id{SPACE}={SPACE}{jpro_id}\n"
+        f"{2 * SPACE}to{SPACE}={SPACE}{resource_type.value}.{resource_type.value}-{jpro_id}\n"
         f"}}\n"
     )
 
